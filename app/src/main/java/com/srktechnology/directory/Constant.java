0b9e2b7f6d3db7c4a56769cbb24b38a899d4d938
SR512 <itplanet99@gmail.com>
2018-07-23 01:11:10 -0700

    CUSTOM FONT APPLAY

---
+++ app/src/main/java/com/srktechnology/directory/Constant.java
@@ -0,0 +1,29 @@
+package com.srktechnology.directory;
+
+/**
+ * Created by SRk on 1/8/2018.
+ */
+
+public class Constant {
+
+    public static final String SERVER = "http://www.srktechnology.in/api/auth/";
+    public static final String API_LOGIN = SERVER + "Login";
+    public static final String API_USERCHECK = SERVER + "UserCheck";
+    public static final String API_REGISTER = SERVER + "UserRegister";
+    public static final String API_NEWUSER_REGISTER = SERVER + "NewRegister";
+    public static final String API_PROFILE = SERVER + "Profile";
+    public static final String API_Advertisement = SERVER + "Advertisement";
+    public static final String API_EVENT = SERVER + "EVENT";
+    public static final String API_Article = SERVER + "Article";
+
+    public static final String PROFILE_PATH = "http://www.srktechnology.in/public/profile/";
+
+    //    Custom Font
+
+    public static final String Poppins_ExtraLight = "fonts/Poppins-ExtraLight.ttf";
+    public static final String Poppins_Light = "fonts/Poppins-Light.ttf";
+    public static final String Poppins_Medium = "fonts/Poppins-Medium.ttf";
+    public static final String Poppins_Regular = "fonts/Poppins-Regular.ttf";
+
+
+}
--- app/src/main/java/com/srktechnology/directory/login_signup_AC.java
+++ app/src/main/java/com/srktechnology/directory/login_signup_AC.java
@@ -1,16 +1,22 @@
 package com.srktechnology.directory;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class login_signup_AC extends AppCompatActivity {
 
-    Button btnSignin,btnSignup;
+    Button btnSignin,btnSignup,btnLogin,btnCheck,btnForgetPassword;
     View vwSignin,vwSignup;
+    EditText edt_Username,edt_Password,edt_Mobile;
+    Typeface Poppins_ExtraLight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,31 @@ public class login_signup_AC extends AppCompatActivity {
         getWindow().setFlags(1024, 1024);
         setContentView(R.layout.activity_login_signup__ac);
 
+       Poppins_ExtraLight  = Typeface.createFromAsset(getAssets(),  Constant.Poppins_ExtraLight);
+
         btnSignin = (Button)findViewById(R.id.btnSignin);
         btnSignup = (Button)findViewById(R.id.btnSignup);
+        btnCheck = (Button)findViewById(R.id.btnLogin);
+        btnLogin = (Button)findViewById(R.id.btnCheck);
+        btnForgetPassword = (Button)findViewById(R.id.btnForgetPassword);
+
         vwSignin  = (View)findViewById(R.id.vw_signin);
         vwSignup  = (View)findViewById(R.id.vw_signup);
+
+        edt_Username = (MaterialEditText)findViewById(R.id.edit_UserName);
+        edt_Password = (MaterialEditText)findViewById(R.id.edit_Password);
+        edt_Mobile = (MaterialEditText)findViewById(R.id.edit_Mobile);
+
+        edt_Username.setTypeface(Poppins_ExtraLight);
+        edt_Password.setTypeface(Poppins_ExtraLight);
+        edt_Mobile.setTypeface(Poppins_ExtraLight);
+
+        btnSignup.setTypeface(Poppins_ExtraLight);
+        btnSignin.setTypeface(Poppins_ExtraLight);
+        btnLogin.setTypeface(Poppins_ExtraLight);
+        btnCheck.setTypeface(Poppins_ExtraLight);
+        btnForgetPassword.setTypeface(Poppins_ExtraLight);
+
 
 
     }
